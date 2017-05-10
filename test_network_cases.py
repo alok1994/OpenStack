70ac42152927431b5ac118a3410cab9a862649fd
alok <verma.alok606@gmail.com>
2017-05-10 03:54:38 -0700

    Add Subnet Cases

--- test_network_cases.py
+++ test_network_cases.py
@@ -8,6 +8,8 @@ import util
 
 tenant_name = 'admin'
 network = 'Net1'
+subnet_name = "Snet1"
+subnet = "10.2.0.0/24"
 
 class TestOpenStackCases(unittest.TestCase):
     @classmethod
@@ -15,7 +17,8 @@ class TestOpenStackCases(unittest.TestCase):
 	pass
 
     @pytest.mark.run(order=1)	
-    def test_Add_network_OpenStack_side(self):
+    def test_Add_Network_with_Subnet_OpenStack_side(self):
         proc = util.utils(tenant_name)
         proc.create_network(network)
+	proc.create_subnet(network, subnet_name, subnet)
 	
--- util.py
+++ util.py
@@ -2,19 +2,10 @@ from novaclient.client import Client
 from neutronclient.v2_0 import client
 import json
 import wapi_module 
-import os
+import os, sys
 import ConfigParser
 import logging 
 
-#CONF = "config.ini"
-#parser = ConfigParser.SafeConfigParser()
-#parser.read(CONF)
-#GRID_VIP = parser.get('Default', 'GRID_VIP')
-#USERNAME = parser.get('Default', 'USERNAME')
-#PASSWORD = parser.get('Default', 'PASSWORD')
-#ADMIN_USERNAME = parser.get('Default', 'ADMIN_USERNAME')
-#ADMIN_PASSWORD = parser.get('Default', 'ADMIN_PASSWORD')
-#WAPI = parser.get('Default', 'WAPI_VERSION')
 os.system('export OS_USERNAME=admin')
 os.system('export OS_PASSWORD=admin')
 os.system('export OS_TENANT_NAME=admin')
@@ -37,20 +28,46 @@ class utils:
         self.neutron_client = client.Client(**neutron_credentials)
 
     def create_network(self,network_name, external=False):
+	''''
+	Add a Network OpenStack Side
+	    Pass the Network Name as Arg
+	'''
         nw = {'network': {'name': network_name, 'admin_state_up': True, 'router:external' : external}}
         netw = self.neutron_client.create_network(body=nw)
         net_dict = netw['network']
         network_id = net_dict['id']
  #   logger.info("Created Network '%s'", network_name)
   #  logger.debug("Network ID of '%s' : %s", network_name, network_id)
-
+    
     def get_networks(self):
+        """
+        Return List of Networks
+        """
         netw = self.neutron_client.list_networks()
         return netw['networks']
 
     def get_net_id(self, nw_name):
+        """
+        Return Network ID for the given Network name
+        """
         nw = self.get_networks()
         for n in nw:
             if n['name'] == nw_name:
                 return n['id'], n['tenant_id']
         return None
+
+    def create_subnet(self, network_name, subnet_name, subnet):
+        """
+        Creates a Subnet
+        It takes Network Name, Subnet Name and Subnet as arguments.
+        For Example:-
+        project.create_subnet("Network1", "Subnet-1-1", "45.0.0.0/24")
+        """
+        net_id, tenant_id = self.get_net_id(network_name)
+        body_create_subnet = {'subnets': [{'name': subnet_name, 'cidr': subnet, 'ip_version': 4, 'tenant_id': tenant_id, 'network_id': net_id}]}
+        try:
+            subnet = self.neutron_client.create_subnet(body=body_create_subnet)
+            logger.info("Created Subnet '%s' under the Network '%s'", subnet_name, network_name)
+        except:
+            print("Failed to create Subnet : ", sys.exc_info()[0])
+
