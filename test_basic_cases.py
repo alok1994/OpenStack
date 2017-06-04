9a165a1ae8fec35398849add299d50219f9fec5d
alok <verma.alok606@gmail.com>
2017-06-04 02:21:29 -0700

    Added routers validation cases

--- test_basic_cases.py
+++ test_basic_cases.py
@@ -6,11 +6,12 @@ import re
 import time
 import util
 import os
+from netaddr import IPNetwork
 
 tenant_name = 'admin'
 network = 'Network'
 subnet_name = "Snet"
-subnet = "20.2.0.0/24"
+subnet = "10.2.0.0/24"
 
 class TestOpenStackCases(unittest.TestCase):
     @classmethod
@@ -66,6 +67,26 @@ class TestOpenStackCases(unittest.TestCase):
                EAs['extattrs']['Network Encap']['value'] == 'vxlan' 
 
     @pytest.mark.run(order=6)
-    def test_Validate_Router(self):
-	pass
+    def test_validate_NIOS_Tenant_ID_Tenant_Name(self):
+	proc = wapi_module.wapi_request('GET',object_type = 'network',params="?network="+subnet)
+        session = util.utils(tenant_name)
+        tenant = session.get_net_id(network)
+        tenant_ID = tenant[1]
+	resp = json.loads(proc)
+        ref_v = resp[0]['_ref']
+        EAs = json.loads(wapi_module.wapi_request('GET',object_type = ref_v + '?_return_fields=extattrs'))
+        assert EAs['extattrs']['Tenant ID']['value'] == tenant_ID and \
+	       EAs['extattrs']['Tenant Name']['value'] == 'admin'
 
+    @pytest.mark.run(order=7)
+    def test_Validate_Router(self):
+	proc = wapi_module.wapi_request('GET',object_type = 'network',params="?network="+subnet)
+	resp = json.loads(proc)
+        ref_v = resp[0]['_ref']
+	options = json.loads(wapi_module.wapi_request('GET',object_type = ref_v + '?_return_fields=options'))
+	route_list = options['options']
+	list_route = route_list[1]
+	route_nios = list_route['value']
+	ip = IPNetwork(subnet).iter_hosts()
+        route = str(ip.next())
+	assert route_nios == route
